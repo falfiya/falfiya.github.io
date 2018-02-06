9e147651825957ef3c98b178af1889992f7d10f1
Coalpha <arcanehood@gmail.com>
2018-02-06 04:35:20 -0800

    Burrows-Wheeler Transform & min!

--- burrows-wheeler-transform/bwt.js
+++ burrows-wheeler-transform/bwt.js
@@ -1,5 +1,16 @@
-function transform() {
-  const s = '\u0000';
-  const $ = '\uffff';
-  
-}
+const s = '\0';
+const e = '퟿';
+function transform(string) {
+  return Array.prototype.map.bind(s + string + e)((_, idx, str) => str.slice(-idx) + str.slice(0, -idx))
+    .sort()
+    .map(permutation => permutation[string.length + 1])
+    .join``;
+};
+function untransform(string) {
+  let ary = string.split``;
+  let oary = string.split``.sort();
+  while(oary[0].length < string.length) {
+    oary = ary.map((val, idx) => val + oary[idx]).sort();
+  }
+  return oary[string.length - 1].slice(2);
+};
