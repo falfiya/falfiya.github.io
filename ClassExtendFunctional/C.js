cdcb81213756a51e2192a63366a1ca29ef9f6517
Acolyte <arcanehood@gmail.com>
2018-04-24 23:15:58 -0700

    Class Based is kinda done!

--- ClassExtendFunctional/C.js
+++ ClassExtendFunctional/C.js
@@ -1,55 +1,89 @@
 // instanceof and C.js don't get along
 const C = {};
-C.class = (name, proto) => {
+C.curry = (fn) => {
+  const argnames = fn.toString().match(/([.\w]+,*\s*)+/)[0].split`,`.map(v => v.replace(/\s/, ''));
+  // Marcus code
+  const len = argnames.length;
+  const args = [];
+  let rest = [];
+  let rcvd = 0;
+  const obj = function (...args) {
+    return fn(...args);
+  };
+  // Now you can call it normally
+  const mayberun = (argname) => {
+    if ((obj.__length__ = ++rcvd) === len) {
+      return fn.apply(0, args.concat(rest));
+    }
+    return obj;
+  };
+  argnames.forEach((argname, idx) => {
+    let a = argname;
+    if (argname[0] === '.') {
+      // if it's a gather
+      a = argname.slice(3);
+      // Slice off the "..."
+      Object.defineProperty(obj, a, {
+        value: (...a) => {
+          rest = a;
+          return mayberun(argname);
+        },
+      });
+    } else {
+      Object.defineProperty(obj, a, {
+        value: (a) => {
+          args[idx] = a;
+          return mayberun(argname);
+        },
+      });
+    }
+    obj[a].displayName = argname;
+  });
+  return obj;
+};
+C.class = C.curry((name, proto) => {
   const classInstance = function (...args) {
     if (!(this instanceof classInstance)) {
       // if the classInstance is not being called with new
-      return new classInstance(...args);
+      const cI = new classInstance(...args);
+      Object.setPrototypeOf(cI, proto);
+      // Add stuff onto the prototype chain
+      return cI;
       // this return blocks all further code
     }
     if (proto.hasOwnProperty('constructor')) {
       proto.constructor.apply(this, args);
     }
   };
-  Object.setPrototypeOf(classInstance, proto);
-  // when new is called on the classInstance it adds
-  // the prototype object to the prototype chain above
   classInstance.displayName = name;
   // assign some properties to the classInstance
-  classInstance.extend = (name, nproto) => Class(name, Object.setPrototypeOf(nproto, proto));
+  classInstance.extend = (nam, nproto) => C.class(nam, Object.setPrototypeOf(nproto, proto));
   classInstance.static = obj => Object.assign(classInstance, obj);
   return classInstance;
-};
-// Class: obj => fn => fn => obj
-C.curry = (fn) => {
-  const argnames = fn.toString().match(/([.\w]+,*\s*)+/g)[0].split`,`.map(v => v.replace(/\s/, ''));
-  // Marcus code
-  const len = argnames.length;
-  const args = [];
-  let rest = [];
-  let rcvd = 0;
-  const obj = {};
-  const mayberun = (argname) => {
-    if (++rcvd === len) {
-      return fn.apply(0, args.concat(rest));
-    }
-    delete obj[argname];
-    return obj;
-  };
-  argnames.forEach((argname, idx) => {
-    if (argname[0] === '.') {
-      // if it's a gather
-      obj[argname.slice(3)] = (...a) => {
-        rest = a;
-        return mayberun(argname);
-      };
-    } else {
-      obj[argname] = (a) => {
-        args[idx] = a;
-        return mayberun(argname);
-      };
-    }
-    obj[argname].displayName = argname;
+});
+const Animal = C.class('Animal', {
+  constructor(a, b) {
+    this.v = a + b;
+  },
+  speak(str) {
+    console.log(str);
+  },
+});
+const Cat = Animal.extend('Cat', {
+  meow() {
+    this.speak('Meow!');
+  },
+});
+const Human = C.class.name('Human')
+  .proto({
+    isStupid: true,
+    constructor(iq) {
+      if (iq > 300) {
+        this.inStupid = false;
+      }
+    },
+    icceAnother() {
+      console.log('Someone got icced');
+      return this;
+    },
   });
-  return obj;
-};
