ec6dd742128c749a7e6ab0ba840ce779eb238891
Acolyte <arcanehood@gmail.com>
2018-05-22 14:39:25 -0700

    Update .hyper.js config file

--- .hyper.js
+++ .hyper.js
@@ -147,18 +147,19 @@ module.exports = {
   //   `@company/project`
   //   `project#1.0.1`
   plugins: [
-    'hyperterm-material',
-    // 'hyper-chesterish',
-    'hyperterm-tabs',
+    // 'hyperterm-material',
+    'hyper-chesterish',
+    'hyper-tabs-enhanced',
     'hyperterm-summon',
-    'hyper-tab-icons',
     'hyperpower',
   ],
 
   // in development, you can create a directory under
   // `~/.hyper_plugins/local/` and include it here
   // to load it and avoid it being `npm install`ed
-  localPlugins: [],
+  localPlugins: [
+
+  ],
 
   keymaps: {
     // Example
