4f6b50ec7a0a66943243b05a3856710ff632497e
Acolyte <arcanehood@gmail.com>
2018-06-06 21:21:56 -0700

    Save stuff?

--- ytdl-mp3/index.js
+++ ytdl-mp3/index.js
@@ -2,12 +2,14 @@
 const fs = require('fs');
 const ytdl = require('ytdl-core');
 const minimist = require('minimist');
-const ffmpeg = require('fluent-ffmpeg')
+const ffmpeg = require('fluent-ffmpeg');
+
 console.log('This may take awhile...');
 
 const args = minimist(process.argv.slice(2), { default: { format: 'mp3' } });
 const url = args._[0];
+const name = args._[1] || 'audio.mp3';
 const reader = ytdl(url, { filter: 'audioonly' });
-reader.pipe(fs.createWriteStream('video.mp4'));
+const out = fs.createWriteStream(name);
 const writer = ffmpeg(reader).format(args.format);
-writer.output(process.stdout).run();
+writer.output(out).run();
