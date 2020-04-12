4be2c17e5c545aa16a088bc61635e908bf5db594
Coalpha <arcanehood@gmail.com>
2020-04-12 00:55:02 -0700

    And nim lived for like, five minutes
    It's getting moved to lang-archives

--- nim/favorite_letter.nim
+++
@@ -1,9 +0,0 @@
-import rdstdin
-
-while true:
-   let letter = readLineFromStdin "What's your favorite letter?\n> "
-   if letter.len > 1:
-      echo letter & " is my favorite letter!"
-      break
-   else:
-      echo "Your letter is too short. Try again"
