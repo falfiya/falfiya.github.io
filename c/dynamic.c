c26260e0af3a6815eb42045efe784cb38d573a4f
coalpha <22552895+coalpha@users.noreply.github.com>
2020-08-25 04:38:00 -0700

    dynamic.c
    It works and I've made something really cursed.

--- c/dynamic.c
+++ c/dynamic.c
@@ -43,7 +43,7 @@ typedef struct VTable_s {
 } VTable_t;
 
 Any_t make_err(char *msg) {
-   
+   puts(msg);
 }
 
 Any_t call_nothing(Any_t self, char *method_name, ...) {
@@ -52,8 +52,11 @@ Any_t call_nothing(Any_t self, char *method_name, ...) {
 
 Any_t call_with(VTable_t table, Any_t self, char *method_name, va_list forwarded_args) {
    TABLE_MAX i = table.length;
-   Method_t current_method = table.methods[i];
+   Method_t current_method;
+   printf("table.length = %hu\n", table.length);
    while (i --> 0) {
+      current_method = table.methods[i];
+      printf("current_method.name = %s\n", current_method.name);
       if (strcmp(current_method.name, method_name) == 0) {
          return current_method.fn(self, forwarded_args);
       }
@@ -62,29 +65,19 @@ Any_t call_with(VTable_t table, Any_t self, char *method_name, va_list forwarded
    return make_err("Could not find that method name");
 }
 
-Any_t ll_say_self(Any_t self, va_list _) {
-   printf("%lld\n", self.as.ll);
-}
-
-VTable_t LL_table = {
-   .name = "long long",
-   .methods = (Method_t[]) {
-      (Method_t) {
-         .name = "say_self",
-         .fn = ll_say_self,
-      }
-   },
-};
+VTable_t *LL_table;
 
 Any_t call_ll(Any_t self, char *method_name, ...) {
+   printf("call_ll(, %s)\n", method_name);
    va_list forwarded_args;
    va_start(forwarded_args, method_name);
-   Any_t res = call_with(LL_table, self, method_name, forwarded_args);
+   Any_t res = call_with(*LL_table, self, method_name, forwarded_args);
    va_end(forwarded_args);
    return res;
 }
 
 Any_t make_ll(long long ll) {
+   printf("make_ll(%lld)", ll);
    return (Any_t) {
       .type = TYPE_LL,
       .as = (Prim_t) ll,
@@ -92,7 +85,37 @@ Any_t make_ll(long long ll) {
    };
 }
 
+Any_t ll_say_self(Any_t self, va_list _) {
+   printf("%lld\n", self.as.ll);
+}
+
+Method_t ll_say_self_m = {
+   .name = "say_self",
+   .fn = ll_say_self,
+};
+
+Any_t ll_add(Any_t self, va_list args) {
+   return make_ll(self.as.ll + va_arg(args, Any_t).as.ll);
+}
+
+Method_t ll_add_m = {
+   .name = "add",
+   .fn = ll_add,
+};
+
 int main() {
-   Any_t one = make_ll(1);
-   one.call(one, "say_self");
+   // setup the vtable
+   LL_table = &(VTable_t) {
+      .name = "long long",
+      .methods = (Method_t[]) {
+         ll_say_self_m, ll_add_m,
+      },
+      .length = 2,
+   };
+
+   Any_t a = make_ll(22);
+   Any_t b = make_ll(47);
+
+   Any_t c = a.call(a, "add", b);
+   c.call(c, "say_self");
 }
