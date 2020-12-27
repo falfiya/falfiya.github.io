6f4f88308b41b85f8c66082203880b40cef21643
coalpha <22552895+coalpha@users.noreply.github.com>
2020-12-26 22:24:28 -0800

    static constexpr explicit const noexcept
           :pinching_hand:
       constexpr
    constexpr
       constexpr
    
      constexpr

--- cxx/box.cxx
+++ cxx/box.cxx
@@ -1,45 +1,47 @@
 // CIS250
 // Doing a dumb school assignment for a class I've never been in
+// Learning how to do genericish programming as well with this
 #include <iostream>
 using namespace std;
 
 #define get(F, f,ield) \
-   inline int get##F##ield() const noexcept { return this->f##ield; }
+   constexpr int get##F##ield() const noexcept { return this->f##ield; }
 #define set(F, f,ield) \
    inline void set##F##ield(int _) noexcept { this->f##ield = _; }
 
 class Box {
-   int width;
-   int height;
-   int depth;
+   int width{};
+   int height{};
+   int depth{};
 public:
-   explicit Box() = default;
-   explicit Box(int w, int h, int d):
+   constexpr explicit Box() = default;
+   constexpr explicit Box(int w, int h, int d):
       width(w),
       height(h),
       depth(d)
    {}
 
+   static constexpr auto NON_ZERO_WARNING{"(not all dimensions are nonzero) "};
    // [4:09 PM] cqwrteur: member functions inside classes are inline by default
    // [4:09 PM] cqwrteur: but marking every function as inline is a simple rule
-   inline int calcArea() const noexcept {
-      if (width & height & depth) {
-         return 0
-            + (2 * width * height)
-            + (2 * depth * height)
-            + (2 * width * depth )
-            ;
+   constexpr int calcArea() const noexcept {
+      auto area{0
+         + (2 * width * height)
+         + (2 * depth * height)
+         + (2 * width * depth )
+      };
+      if (!area) {
+         cout << NON_ZERO_WARNING;
       }
-      cout << "Not all dimensions are nonzero.\n";
-      return 0;
+      return area;
    }
 
-   inline int calcVolume() const noexcept {
-      if (width & height & depth) {
-         return depth * width * height;
+   constexpr int calcVolume() const noexcept {
+      auto volume{depth * width * height};
+      if (!volume) {
+         cout << NON_ZERO_WARNING;
       }
-      cout << "Not all dimensions are nonzero.\n";
-      return 0;
+      return volume;
    }
 
    get(W, w,idth);
@@ -54,12 +56,12 @@ public:
 int main() {
    cout << "Name:   Cole Gannon -  Program Name:  Box -  Date: 122520\n";
    // Box 1 - Test Parameterized constructor, area of sides and volume functions
-   Box B1(1, 2, 3);
+   constexpr Box B1(1, 2, 3);
    cout << ""
       "Width  = " << B1.getWidth()   << "\n"
       "Area   = " << B1.calcArea()   << "\n"
       "Volume = " << B1.calcVolume() << "\n"
-   ;
+   "\n";
 
    // Box 2 - Test set functions, Volume, getHeight and area functions
    Box B2;
@@ -71,7 +73,7 @@ int main() {
       "Height = " << B2.getHeight()  << "\n"
       "Area   = " << B2.calcArea()   << "\n"
       "Volume = " << B2.calcVolume() << "\n"
-   ;
+   "\n";
 
    // Box 3 - Test zero value error for calc Area and Volume of sides functions
    Box B3;
