8c1a7256db1e2eba1303056b5b428a1611f2d945
coalpha <22552895+coalpha@users.noreply.github.com>
2021-02-12 16:56:14 -0800

    Now I am become cxx...
    This code could be hung in an art exhibition and people would walk
    up to it and say "who's this Gogh guy to the left of this?"

---
+++ cxx/ary.cxx
@@ -0,0 +1,65 @@
+#include <new>
+#include <initializer_list>
+#include <utility>
+#include <iterator>
+using namespace std;
+
+using byte_t = char;
+
+template <class T, size_t len_t = 0>
+struct ary {
+   size_t len;
+   T data[len_t];
+   constexpr explicit ary() noexcept: len{len_t} {}
+   constexpr explicit ary(size_t const len) noexcept: len{len} {}
+
+   /*
+   Would really like homogeneous variadic function parameters:
+   http://www.open-std.org/jtc1/sc22/wg21/docs/papers/2019/p1219r2.html
+   */
+   template <size_t i, size_t len, class ...Ts>
+   static constexpr void init_array(T *array, T &&first, Ts &&...args) noexcept {
+      array[i] = first;
+
+      if constexpr (i + 1 < len) {
+         init_array<i + 1, len>(array, forward<Ts>(args)...);
+      }
+   }
+
+   template <class ...Ts, typename enable_if<sizeof...(Ts) == len_t, size_t>::type = 0>
+   constexpr ary(Ts &&...rest) noexcept: len{len_t} {
+      init_array<0, len_t>(this->data, forward<Ts>(rest)...);
+   }
+
+   static constexpr ary<T, len_t> *heap() noexcept {
+      constexpr size_t bytes{sizeof(ary) + len_t * sizeof(T)};
+      byte_t *const buffer{new byte_t[bytes]};
+      return new(buffer) ary<T, len_t>();
+   }
+
+   static inline ary<T> *heap(size_t const len) noexcept {
+      size_t const bytes{sizeof(ary) + len * sizeof(T)};
+      byte_t *const buffer{new byte_t[bytes]};
+      return new(buffer) ary<T>(len);
+   }
+
+   template <class ...Ts>
+   static constexpr ary<T> *heap(Ts &&...rest) noexcept {
+      constexpr size_t len{sizeof...(rest)};
+      ary *const self{ary<T>::heap(len)};
+      init_array<0, len>(self->data, forward<Ts>(rest)...);
+      return self;
+   }
+
+   constexpr T *begin() noexcept { return this->data; }
+   constexpr T *end() noexcept { return this->data + len; };
+};
+
+#include <iostream>
+
+int main() {
+   // would be nice if you didn't have to declare the size but whatever
+   ary<int, 4> stack = {1, 2, 3, 4};
+   auto *heap{ary<int>::heap(9, 10, 11, 3, 4)};
+   cout << heap->len << "\n";
+}
