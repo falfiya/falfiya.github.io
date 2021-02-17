8e04c0c9f3c5f2f9c084def591489a9dd43bd904
coalpha <22552895+coalpha@users.noreply.github.com>
2021-02-17 04:05:17 -0800

    so close yet so far

--- ts/fs_path.ts
+++ ts/fs_path.ts
@@ -1,13 +1,13 @@
-declare const url_path: unique symbol;
+declare const fs_path_s: unique symbol;
 
 type index_holder<path extends string, index extends number> =
    {[i in index]: path};
 
 type fs_path<path extends string, index extends number> =
-   string & {readonly [url_path]: index_holder<path, index>};
+   string & {readonly fs_path: index_holder<path, index>};
 
 type usr_p = fs_path<"usr", 0>;
-type etc_p = fs_path<"etc",  0>;
+type etc_p = fs_path<"etc", 0>;
 
 type nonsense_p = usr_p & etc_p;
 type nonsense_p_is_never = nonsense_p extends never ? true : false;
@@ -18,4 +18,4 @@ type etc_i = index_holder<"etc", 0>;
 type nonsense_i = usr_i & etc_i;
 type nonsense_i_is_never = nonsense_i extends never ? true : false;
 
-type what = nonsense_i_is_never extends nonsense_p_is_never ? true : false;
+const a: number = "" as never;
---
+++ ts/sigh.ts
@@ -0,0 +1,13 @@
+// ARGH NO SFINAE
+// HOW DO I MAKE THINGS THROW AN ERROR??!?!
+
+type enable_if<cond, T> = cond extends true ? T : void;
+type extend<T, U>  = T extends U ? true : false;
+type not<T> = T extends true ? false : true;
+
+function add<T, U extends enable_if<not<extend<T, string>>, number>>(a: T, b: U): T {
+   return a as any + b;
+}
+
+add(1, 2);
+add("3", "4");
