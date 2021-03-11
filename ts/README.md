6f926e867b02a918a4644b0b75162bfa98c6c800
coalpha <22552895+coalpha@users.noreply.github.com>
2021-03-11 02:33:57 -0800

    typelevel unwrap1 and ts/README.md
    All I had to do to get typelevel unwrap working (which was the goal
    in the first place) was to remove that typeof in front of the
    object access.

---
+++ ts/README.md
@@ -0,0 +1,19 @@
+# typescript
+
+TypeScript gives me blue balls. It's like almost good enough and nearly has all
+of the type power that you want and then it falls flat on it's face. MICROSOFT
+is *teasing* me with this stuff. Give me my compile time generic programming.
+C++ templates aren't *that* bad, are they?
+
+## give me
+
+<sub>in no particular order</sub>
+
+- typelevel number arithmetic
+- string introspection and mapping at compile time
+- higher kinded types
+- proper newtypes
+- a way to generate compile time errors
+- `infer T` without `extends?:`
+- uint, int, float
+- optimizing compiler
--- ts/unwrap1.ts
+++ ts/unwrap1.ts
@@ -6,6 +6,7 @@ const make_fridge = <val extends string>(val: val) => val as val & fridge;
 
 const frigidaire = make_fridge("Frigidaire");
 const kitchenaid = make_fridge("KitchenAid");
+declare const unknown_fridge: fridge;
 
 type washer_t = {readonly [appliance_symbol]: washer_t};
 type washer = string & washer_t;
@@ -13,8 +14,18 @@ const make_washer = <val extends string>(val: val) => val as val & washer;
 
 const ge = make_washer("GE");
 const samsung = make_washer("Samsung");
+declare const unknown_washer: washer;
 
 const unwrap_appliance = <T extends {readonly [appliance_symbol]: unknown}>(T: T) =>
    T as T extends infer val & typeof T[typeof appliance_symbol] ? val : never;
 
-const f = unwrap_appliance(frigidaire);
+const frigidaire_name     = unwrap_appliance(frigidaire);
+const ge_name             = unwrap_appliance(ge);
+const unknown_fridge_name = unwrap_appliance(unknown_fridge);
+
+type unwrap_appliance<T extends {readonly [appliance_symbol]: unknown}> =
+   T extends infer val & T[typeof appliance_symbol] ? val : never;
+
+type kitchenaid_name     = unwrap_appliance<typeof kitchenaid>;
+type samsung_name        = unwrap_appliance<typeof samsung>;
+type unknown_washer_name = unwrap_appliance<typeof unknown_washer>;
