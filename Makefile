91fec9aea9ffd5512e01297a32973f0cf239f798
coalpha <22552895+coalpha@users.noreply.github.com>
2021-03-22 03:04:24 -0700

    Makefile and CoContravariance
    My titles aren't very helpful.
    - Patched up the makefile with the xplatform support that I built
      into my cs class base makefile.
    - Discovered the point of Contravariance, I suppose.

--- Makefile
+++ Makefile
@@ -1,56 +1,63 @@
-space_cat.exe: c/space_cat.c
+ifeq ($(OS), Windows_NT)
+	exe := exe
+	cxx_flags += -fuse-ld=lld-link
+else
+	exe := elf
+endif
+
+space_cat.$(elf): c/space_cat.c
 	clang $< -Wall -Wextra -Ofast -fuse-ld=lld -o $@
 
 # c
-c_flags  := $(shell space_cat.exe c/compile_flags.txt)
+c_flags  := $(shell space_cat.$(elf) c/compile_flags.txt)
 c_dflags := -g
 c_rflags := -O2
 
 ## base
-c/%.exe: c/%.c
+c/%.$(elf): c/%.c
 	-clang $< $(c_flags) -o $@
 
-run~%.c: c/%.exe
+run~%.c: c/%.$(elf)
 	@-$<
 
 ## debug
-c/%.debug.exe: c/%.c
+c/%.debug.$(elf): c/%.c
 	-clang $< $(c_flags) $(c_dflags) -o $@
 
-debug~%.c: c/%.debug.exe
+debug~%.c: c/%.debug.$(elf)
 	-
 
 ## release
-c/%.release.exe: c/%.c
+c/%.release.$(elf): c/%.c
 	-clang $< $(c_flags) $(c_rflags) -o $@
 
-release~%.c: c/%.release.exe
+release~%.c: c/%.release.$(elf)
 	-
 
 # cxx
-cxx_flags  := $(shell space_cat.exe cxx/compile_flags.txt)
+cxx_flags  := $(shell space_cat.$(elf) cxx/compile_flags.txt)
 cxx_dflags := -g
 cxx_rflags := -O2
 
 ## base
-cxx/%.exe: cxx/%.cxx
+cxx/%.$(elf): cxx/%.cxx
 	-clang++ $< $(cxx_flags) -o $@
 
-run~%.cxx: cxx/%.exe
+run~%.cxx: cxx/%.$(elf)
 	@-$<
 
 ## debug
-cxx/%.debug.exe: cxx/%.cxx
+cxx/%.debug.$(elf): cxx/%.cxx
 	-clang++ $< $(cxx_flags) $(cxx_dflags) -o $@
 
-debug~%.cxx: cxx/%.debug.exe
+debug~%.cxx: cxx/%.debug.$(elf)
 	-
 
 ## release
-cxx/%.release.exe: cxx/%.cxx
+cxx/%.release.$(elf): cxx/%.cxx
 	-clang++ $< $(cxx_flags) $(cxx_rflags) -o $@
 
-release~%.cxx: cxx/%.release.exe
+release~%.cxx: cxx/%.release.$(elf)
 	-
 
 clean-artifacts: run~clean_artifacts.cxx
--- java/CoContraVariance.java
+++ java/CoContraVariance.java
@@ -12,11 +12,16 @@ interface CoContraVariance {
    static void main(final String[] args) {
       final var mixedList = List.<Animal>of(animal, feline, kitten, serval, jackal);
       final var kittens = Collections.nCopies(3, kitten);
+      final var servals = Collections.nCopies(2, serval);
+      final var felines = Collections.nCopies(4, feline);
 
       printAnimalNames0(mixedList);
 
       printAnimalNames1(mixedList); //:: Animal
-      printAnimalNames1(kittens); //:: Kitten
+      printAnimalNames1(kittens);   //:: Kitten
+
+      addServal(servals);
+      addServal(felines);
    }
 
    /// Covariance
@@ -39,7 +44,7 @@ interface CoContraVariance {
       return null;
    }
 
-   static void addServal0(final List<? extends Animal> animals) {
+   static void _addServal(final List<? extends Animal> animals) {
       // cannot do this, what if the list was only of Jackals?
       //animals.add(serval);
    }
@@ -55,19 +60,31 @@ interface CoContraVariance {
    /**
     * This simply does not work since the things within this list can be Object
     */
-   static void printAnimalNames2(final List<? super Animal> animals) {
+   static void _printAnimalNames2final(List<? super Animal> animals) {
       //animals.forEach(Animal::sayName);
    }
 
-   static void addServal2(List<? super Serval> servals) {
+   static void addServal(List<? super Serval> servals) {
       // we can add into the list because the only guarantee is that the lowest
       // object is a serval
       servals.add(serval);
 
       // we could not, for instance, do either of these:
       //servals.add(jackal);
-      // neither are a super class of Serval and it would seem ridiculous to add
-      // a Jackal into a list of Servals.
+      //servals.add(feline);
+      // ok not being able to add Jackals makes sense but I really have no idea
+      // why we're not allowed to add felines. Doesn't the list only guarantee
+      // that the objects inside are above serval? I guess if you look at the
+      // examples on line 23 and 24, you can see that we wouldn't want to, say,
+      // add a Feline into a List<Serval> but adding a Serval into a
+      // List<Feline> is actually fine. So this contravariance isn't actually
+      // that cool inside of the function. It's basically List<Serval> where you
+      // don't actually know what parent it is of the Serval so you can't
+      // actually read any properties on it. However, you can write Servals into
+      // it, which is pretty useful, I suppose. Basically it's only use is at
+      // the call site for allowing stuff like List<Feline> -> List<Serval>. You
+      // do end up sacrificing the reading capabilities but this seem to have a
+      // use of some sort.
    }
 
    // this does not work. for some reason, they have decided not to allow this
