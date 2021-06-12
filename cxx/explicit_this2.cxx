924150fc80a481c19ecb5d28159d596886e75de4
coalpha <22552895+coalpha@users.noreply.github.com>
2021-06-12 00:43:16 -0700

    bit_cast
    reinterpret_cast, our relationship was long and wonderful. We shared
    many intimate moments together but in the end, you refused to let me
    cast a pointer to a member function. reinterpret_cast, I know you
    care about my safety, but frankly, it's not up to you to decide what
    I do. This is *my* foot and I will point *my* gun at it.

---
+++ cxx/explicit_this2.cxx
@@ -0,0 +1,31 @@
+#include <bit>
+#include <iostream>
+using namespace std;
+
+struct duck {
+   int const age;
+   char const *type;
+
+   void info() const noexcept {
+      cout << "The " << this->type << " duck is " << this->age << " years old\n";
+   }
+
+   using info_t = void (*)(void const *self) noexcept;
+};
+
+struct bunny {
+   int const weight;
+   char const *name;
+};
+
+int main() noexcept {
+   printf();
+   auto info{bit_cast<duck::info_t>(&duck::info)};
+
+   duck random_duck{.age = 3, .type = "mallard"};
+   random_duck.info();
+   info(&random_duck);
+
+   bunny flopsy{.weight = 10, .name = "flopsy"};
+   info(&flopsy);
+}
