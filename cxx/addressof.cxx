44d8345d3f0263cc6e7ad3a0abd8911a782dbf76
coalpha <22552895+coalpha@users.noreply.github.com>
2021-08-14 15:12:01 -0700

    __builtin_addressof suddenly makes sense

---
+++ cxx/addressof.cxx
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <type_traits>
+
+struct above_me {
+   size_t dummy;
+   // why is this a thing
+   constexpr void *operator&() const noexcept {
+      return (char *) this + sizeof(above_me);
+   }
+};
+
+int main() noexcept {
+   size_t const volatile five{5};
+   above_me above_me{};
+   auto const above_me_ptr{(std::remove_cv_t<decltype(five)> *) &above_me};
+   *above_me_ptr = 4;
+   std::cout << "five is " << five << '\n';
+}
