b99fb2f96c54f83d8b32f64b5430e52c0196149a
coalpha <22552895+coalpha@users.noreply.github.com>
2021-08-22 00:55:43 -0700

    std::bitcast in Java. suffer.

--- java/Safety/Bitcast.java
+++ java/Safety/Bitcast.java
@@ -3,6 +3,12 @@ import sun.misc.Unsafe;
 import java.lang.reflect.Field;
 
 class Bitcast {
+   Object obj;
+   Bitcast(Object obj) {
+      this.obj = obj;
+   }
+   static long objBaseOffset;
+
    static Unsafe unsafe = null;
    static {
       try {
@@ -11,22 +17,23 @@ class Bitcast {
          f = Unsafe.class.getDeclaredField("theUnsafe");
          f.setAccessible(true);
          unsafe = (Unsafe) f.get(null);
-      } catch (Exception any) {}
+
+         f = Bitcast.class.getDeclaredField("obj");
+         objBaseOffset = unsafe.objectFieldOffset(f);
+      } catch (Exception any) {
+         System.exit(1);
+      }
    }
 
    static Object longToObject(long lng) {
-      final Object[] qwordPtr = {null};
-      final long baseOffset = unsafe.arrayBaseOffset(Object[].class);
-      unsafe.putLong(qwordPtr, baseOffset, lng);
-      return qwordPtr[0];
+      final var qwordPtr = new Bitcast(null);
+      unsafe.putLong(qwordPtr, objBaseOffset, lng);
+      return qwordPtr.obj;
    }
 
    static long objectToLong(Object obj) {
-      // irritating, the jvm will dereference obj when it is put into an array
-      // this means that the code won't work
-      Object[] qwordPtr = {obj};
-      final long baseOffset = unsafe.arrayBaseOffset(Object[].class);
-      return unsafe.getLong(qwordPtr, baseOffset);
+      final var qwordPtr = new Bitcast(obj);
+      return unsafe.getLong(qwordPtr, objBaseOffset);
    }
 
    public static void main(String[] args) {
