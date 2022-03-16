41c1d73b91f6448a38f4b713d1e35abb9bd6881e
coalpha <22552895+coalpha@users.noreply.github.com>
2022-03-15 22:04:27 -0700

    .net is confusing

--- .vscode/alive/fasl/tmp.lisp
+++
@@ -1,3 +0,0 @@
-(defun read-lines (fd)
-   (let ((line (read-line fd nil)))
-      (and line (append (list line) (read-lines fd)))))
---
+++ cs/Hello/Hello.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+internal class Hello
+{
+   static void Main(string[] args)
+   {
+      Console.WriteLine("Hello World!");
+   }
+}
