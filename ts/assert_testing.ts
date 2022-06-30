f9562f428cc1c52e35a150e057a31f9cba374696
coalpha <22552895+coalpha@users.noreply.github.com>
2022-06-29 23:18:24 -0700

    this seems to work

---
+++ ts/assert_testing.ts
@@ -0,0 +1,13 @@
+type assert_obj<T> = {
+   assert(u: unknown): asserts u is T;
+   typename: string;
+};
+
+const object_assert: assert_obj<{}> = {
+   assert(u: unknown): asserts u is ({}) {
+      if (typeof u !== "object" || u === null) {
+         throw new TypeError("Could not assert value is non-null object!");
+      }
+   },
+   typename: "non-null object",
+};
