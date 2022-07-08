4f0dfeb05b5735ac60fb16ce91c745cf748b13e9
coalpha <22552895+coalpha@users.noreply.github.com>
2022-07-07 19:23:18 -0700

    nisu

--- ts/strnum.ts
+++ ts/strnum.ts
@@ -4,30 +4,52 @@ type char = digits[number];
 type char_except_0 = "1" | "2" | "3" | "4" | "5" | "6" | "7" | "8" | "9";
 type char_except_9 = "0" | "1" | "2" | "3" | "4" | "5" | "6" | "7" | "8";
 
-type head<s> = s extends `${infer head}${char}` ? head : "";
-type tail_or<s, or> =
-   s extends `${infer head}${char}`
-      ? s extends `${head}${infer tail}` ? tail : never
-      : or;
 type pred<i extends char_except_0> = [never, ...digits][i];
 
 type add_char<acc, i> =
    i extends char_except_0
       ? acc extends char_except_9
          ? add_char<["1", "2", "3", "4", "5", "6", "7", "8", "9"][acc], pred<i>>
-         : [add_char<"0", pred<i>>[0], true]
-      : [acc, false];
+         : [result: add_char<"0", pred<i>>[0], carry: true]
+      : [result: acc, carry: false];
 
-type do_carry<carry, x> = carry extends true ? add_char<x, "1"> : [x, false];
 type or<a, b> = a | b extends false ? false : true;
 
-type add<acc extends string, i extends string, carry = false> =
+// bootstrapper
+// acc + i = ??
+type add<acc extends string, i extends string> = add$0<i, acc, false>;
+
+type add$0<i extends string, acc extends string, carry> =
    i extends `${infer i_first}${char}`
       ? i extends `${i_first}${infer i_last}`
-         ? `${add<head<acc>, i_first, or<do_carry<carry, i_last>[1], add_char<tail_or<acc, "0">, do_carry<carry, i_last>[0]>[1]>>}${add_char<tail_or<acc, "0">, do_carry<carry, i_last>[0]>[0]}`
+         // If there's a carry from the previous (aka lesser) digit, add one to
+         // our current digit. When our current digit is "9", we overflow to
+         // zero and the carry flag is set.    vvvvv
+         ? add$1<i_first, carry extends true ? add_char<i_last, "1"> : [i_last, false], acc>
          : never
+      // Empty string for the i side but there's a carry so instead of being
+      // done, we have to add one more.
       : carry extends true
-         ? add<acc, "0", true>
-         : acc;
+         ? add$1<"", ["1", false], acc>
+         : acc // <-- exit
 
-export type lol = add<"378", "42">; //: "420"
+// acc:    2
+//   i: + 40
+//
+// We see that the acc side is short on digits when we get to the 10s column.
+// add$1 generates "0" digits for the acc side when needed so that add_char works correctly.
+type add$1<i_first extends string, i_last extends [any, any], acc extends string> =
+   acc extends `${infer acc_first}${char}` ? acc extends `${acc_first}${infer acc_last}`
+      ? add$2<i_first, i_last, acc_first, acc_last> : never
+      : add$2<i_first, i_last, "", "0">
+
+// add$2 is entirely a passthrough lambda so that we have an alias for add_res
+type add$2<i_first extends string, i_last extends [any, any], acc_first extends string, acc_last> =
+   add$3<i_first, i_last, acc_first, add_char<acc_last, i_last[0]>>;
+
+// Figure out if there was any carrying done along the way, recurse, and concatenate strings.
+type add$3<i_first extends string, i_last extends [any, any], acc_first extends string, add_res extends [any, any]> =
+   `${add$0<acc_first, i_first, or<i_last[1], add_res[1]>>}${add_res[0]}`;
+
+
+export type _420 = add<"378", "42">;
