4135be2d7c4ec345210401d625752bae40148a7d
coalpha <22552895+coalpha@users.noreply.github.com>
2022-10-25 12:14:49 -0700

    now these work

--- ts/string_prim.ts
+++ ts/string_prim.ts
@@ -7,31 +7,43 @@ type _not_union<t1, t2> =
          ? true
          : unknown
       : never;
+/** Returns `true` if t is not a union, `unknown` otherwise */
 type not_union<t> = _not_union<t, t>;
 
-type z = `${undefined}`;
+/** Returns `true` on regular string and `false` on templated string */
 type not_templated_string<s extends string> =
-   s extends `${infer head}${infer tail}`
-      ? `${string}` extends head
-         ? false
-         : `${number}` extends head
+   s extends ""
+      ? true
+      : s extends `${infer head}${infer tail}`
+         ? string extends head
             ? false
-            : `${bigint}` extends head
+            : `${string}` extends head
                ? false
-               : `${boolean}` extends head
+               : `${number}` extends head
                   ? false
-                  : not_templated_string<tail>
-      : true;
+                  : `${bigint}` extends head
+                     ? false
+                     : `${boolean}` extends head
+                        ? false
+                        : not_templated_string<tail>
+         : false;
 
+/** Returns `true | false` */
 type is_prim_string<s extends string> =
    not_union<s> extends true
       ? not_templated_string<s>
       : false;
 
+// gotta love typescript
 is_false<is_prim_string<"foo" | "bar">>();
 is_false<is_prim_string<`${number}qux`>>();
 is_false<is_prim_string<`paw${number}`>>();
 is_false<is_prim_string<`baz${string}vug`>>();
 is_false<is_prim_string<`${boolean}`>>();
+is_false<is_prim_string<`${string}`>>();
+is_false<is_prim_string<string>>();
+is_false<is_prim_string<`${string}aaaa${string}`>>();
+is_false<is_prim_string<`aaaasdasd${1 | 2}asdasd`>>();
 is_true<is_prim_string<"floof">>();
 is_true<is_prim_string<`${undefined}${null}`>>();
+is_true<is_prim_string<"">>();
