47c170817f92de9ecb924fa24cdc26a0d5b52ffd
coalpha <22552895+coalpha@users.noreply.github.com>
2022-10-27 17:40:24 -0700

    ok it works properly now
    happy???

--- ts/newtype5.ts
+++ ts/newtype5.ts
@@ -3,10 +3,15 @@ export {};
 
 declare const phi: unique symbol;
 
+type union_to_intersection<u> =
+   (u extends any ? {(k: u): void} : never) extends {(k: infer t): void} ? t : never;
+type newtype_partials_union<phi_uniq_union extends keyof any> =
+   {[k in phi_uniq_union]: {[phi]: {[_ in k]: void}}}[phi_uniq_union];
+
 type newtype<uniq extends string | symbol> = {[phi]: {[k in uniq]: void}};
 type unwrap<outer> =
    outer extends {[phi]: {}}
-      ? outer extends infer inner & {[phi]: {[k in keyof outer[typeof phi]]: void}}
+      ? outer extends infer inner & union_to_intersection<newtype_partials_union<keyof outer[typeof phi]>>
          ? inner
          : never
       : outer;
@@ -20,7 +25,7 @@ function is_unsigned(n: number | bigint): n is unsigned {
 }
 type i32 = number & newtype<"i32">;
 type u32 = i32 & unsigned;
-declare function take_u32(u: u32): void;
+declare function take_u32(u: u32): `seconds: ${unwrap<u32>}`;
 
 declare const my_int: i32;
 if (is_unsigned(my_int)) {
@@ -28,3 +33,5 @@ if (is_unsigned(my_int)) {
 } else {
    take_u32(my_int);
 }
+
+type num = unwrap<u32>; //:: number
