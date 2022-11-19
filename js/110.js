1f8d624bb22a8e726a9bad028338193844d6c98b
coalpha <22552895+coalpha@users.noreply.github.com>
2022-11-19 02:09:24 -0800

    pretty sure this is the smallest 110

---
+++ js/110.js
@@ -0,0 +1,12 @@
+c=(a,b,c)=>b?!(a&c):c
+c=(a,b,c)=>b?!a|!c:c
+r=(a,b,c)=>b&!a|c^b
+
+console.log(c(0, 0, 0), 0)
+console.log(c(0, 0, 1), 1)
+console.log(c(0, 1, 0), 1)
+console.log(c(0, 1, 1), 1)
+console.log(c(1, 0, 0), 0)
+console.log(c(1, 0, 1), 1)
+console.log(c(1, 1, 0), 1)
+console.log(c(1, 1, 1), 0)
