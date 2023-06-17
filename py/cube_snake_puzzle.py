ed64006e5fab5818f4e5d5cc9b258798b152c4b2
coalpha <22552895+coalpha@users.noreply.github.com>
2023-06-16 18:13:51 -0700

    cleaner snake cube output

--- py/cube_snake_puzzle.py
+++ py/cube_snake_puzzle.py
@@ -1,5 +1,7 @@
 from __future__ import annotations
 from typing import *
+from termcolor import colored
+from traceback import print_stack
 
 T = TypeVar("T")
 Fn = TypeVar("Fn", bound=Callable)
@@ -69,16 +71,16 @@ class Cube:
       coords = r3.as_tuple()
       return 0 <= min(coords) and max(coords) < 3
 
-   __index = int
-   def __R3_to_index(r3: R3) -> __index:
+   index = int
+   def to_index(r3: R3) -> index:
       return r3.x + r3.y * 3 + r3.z * 3 * 3
 
    # short tests
-   __test_idx_last: __index = -1
+   __test_idx_last: index = -1
    for z in (0, 1, 2):
       for y in (0, 1, 2):
          for x in (0, 1, 2):
-            new_index = __R3_to_index(R3(x, y, z))
+            new_index = to_index(R3(x, y, z))
             assert new_index > __test_idx_last, "indexes should only increase"
             __test_idx_last = new_index
 
@@ -87,11 +89,11 @@ class Cube:
       self._filled = filled
 
    def fill(self, r3: R3) -> Self:
-      self._filled[Cube.__R3_to_index(r3)] = True
+      self._filled[Cube.to_index(r3)] = True
       return self
 
    def is_filled(self, r3: R3) -> bool:
-      return self._filled[Cube.__R3_to_index(r3)]
+      return self._filled[Cube.to_index(r3)]
 
    def is_empty(self, r3: R3) -> bool:
       return not self.is_filled(r3)
@@ -107,10 +109,7 @@ class Cube:
          if i % 9 == 0:
             out += "      \n"
          if e:
-            if i % 2 == 0:
-               out += "o "
-            else:
-               out += "o "
+            out += "o "
          else:
             out += ". "
       return out
@@ -151,11 +150,31 @@ class CubeHistoryState:
    def __init__(self, steps: steps, cube: Cube):
       self.steps = steps
       self.cube = cube
+
    def __repr__(self) -> str:
-      return "\n".join([
-         *[f"# {step}  #" for step in self.steps],
-         *[f"#   {line}  #" for line in repr(self.cube).splitlines()],
-      ])
+      out = ""
+      step_indexes = tuple(Cube.to_index(step) for step in self.steps)
+      middle_steps = set(step_indexes[:-1])
+      last_step = step_indexes[-1]
+
+      for i, e in enumerate(self.cube._filled):
+         if i % 9 == 0:
+            out += "        \n"
+         if i % 3 == 0:
+            out += f"{i // 9}│"
+         if e:
+            if i == last_step:
+               out += colored("o ", "yellow")
+            elif i in middle_steps:
+               out += colored("x ", "cyan")
+            else:
+               out += "o "
+         else:
+            out += ". "
+         if i % 3 == 2:
+            out += "\n"
+      return out
+
    def copy(self) -> Self:
       return type(self)([], self.cube.copy())
 
@@ -171,21 +190,31 @@ class CubeHistory(list[CubeHistoryState]):
       return self[-1].cube.okay(r3)
 
    def __repr__(self):
-      out = ""
+      state_output: list[str] = []
+      LINE_COUNT = 16
       for i, history_state in enumerate(self):
-         # f"############",
-         # f"# Step  99 #",
-         # f"#   - - -  #",
-         # f"# 0, 0, 3  #,
-         # f"############",
-         out += "\n".join([
-            f"#############",
-            f"#  Step {i + 1:2d}  #",
-            *repr(history_state).splitlines(),
-            f"#############",
-            f"",
-         ])
-      return out
+         out = ""
+         out += f"┌───────────┐\n"
+         out += f"│  Step {i + 1:02d}  │\n"
+         for line in repr(history_state).splitlines():
+            out += f"│  {line} │\n"
+         out += f"│           │\n"
+         out += f"└───────────┘\n"
+         state_output.append(out)
+
+      # print sxs states out side by side
+      sxs = 6
+      actual_out = ""
+      while len(state_output) > 0:
+         window = state_output[:sxs]
+         state_output = state_output[sxs:]
+         current_states = tuple(so.splitlines() for so in window)
+         for current_line in range(0, LINE_COUNT):
+            for current_state in current_states:
+               actual_out += f"{current_state[current_line]} "
+            actual_out += "\n"
+
+      return actual_out
 
    def next(self) -> Self:
       assert len(self) > 0, "CubeHistory should never be empty!"
@@ -195,8 +224,7 @@ class CubeHistory(list[CubeHistoryState]):
       return type(self)(self)
 
 def try_permute(*, turns: list[int], pos: R3, ori: Ori, ch: CubeHistory, po="") -> Optional[list[Cube]]:
-   if len(turns) == 0:
-      return [ch]
+   assert len(turns) > 0, "SANITY: there must be at least one element in turns"
    count = turns[0]
    turns = turns[1:]
    assert count > 0, "count must be > 0"
@@ -204,11 +232,12 @@ def try_permute(*, turns: list[int], pos: R3, ori: Ori, ch: CubeHistory, po="") 
    for _ in range(0, count):
       pos += ori
       if not ch.cu_okay(pos):
-         print(f"{po} died")
          return None
       ch.cu_fill(pos)
-      print(f"{po}{pos} + {ori}")
 
+   # base case
+   if len(turns) == 0:
+      return [ch]
 
    possible_histories = []
    for new_ori in ori.perp():
@@ -219,24 +248,15 @@ def try_permute(*, turns: list[int], pos: R3, ori: Ori, ch: CubeHistory, po="") 
 
    return possible_histories
 
-def base():
-   ch = CubeHistory()
+ch = CubeHistory()
+pos = R3(0, 0, 0)
+ch.cu_fill(pos)
+pos += x
+ch.cu_fill(pos)
+pos += x
+ch.cu_fill(pos)
 
-   pos = R3(0, 0, 0)
-   ch.cu_fill(pos)
-
-   pos += x
-   ch.cu_fill(pos)
-
-   pos += x
-   ch.cu_fill(pos)
-
-   turns = [2, 2, 2, 1, 1, 1, 2, 2, 1, 1, 2, 1, 2, 1, 1, 2]
-   for i, sol in enumerate(try_permute(turns=turns, pos=pos, ori=y, ch=ch.next())):
-      print(f"Solution #{i + 1}")
-      print(sol)
-
-   # print(ch)
-   # try_permute()
-
-base()
+turns = [2, 2, 2, 1, 1, 1, 2, 2, 1, 1, 2, 1, 2, 1, 1, 2]
+for i, sol in enumerate(try_permute(turns=turns, pos=pos, ori=y, ch=ch.next())):
+   print(f"Solution #{i + 1}")
+   print(sol)
