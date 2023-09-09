9041b452b93de1e3176b25f4ec5dd0b646877fcb
coalpha <22552895+coalpha@users.noreply.github.com>
2023-09-09 14:33:14 -0700

    computers <3

--- cxx/addressof_stupidity.cxx
+++ cxx/addressof_stupidity.cxx
@@ -5,10 +5,16 @@ constexpr inline no_address_lol *operator&(no_address_lol &) noexcept {
    return nullptr;
 }
 
+template <typename T>
+constexpr inline T *addressof(T &t) noexcept {
+   return reinterpret_cast<T *>(&reinterpret_cast<char &>(t));
+}
+
 int main() noexcept {
    no_address_lol scrumge;
    std::cout
       << &scrumge << '\n'
       // no std::addressof in freestanding :scrange:
+      << addressof(scrumge) << '\n'
       << __builtin_addressof(scrumge) << '\n';
 }
