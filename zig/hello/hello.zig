66936d7b794849f70f447e68ad8c2508b68381e1
coalpha <22552895+coalpha@users.noreply.github.com>
2023-09-14 14:02:19 -0700

    good progress zig!
    c/hello/main.exe 	- 1088 bytes
    zig/hello/hello.exe	- 1072 bytes

--- zig/hello/hello.zig
+++ zig/hello/hello.zig
@@ -1,14 +1,18 @@
-const w = struct { usingnamespace @import("std").os.windows; };
-const k32 = struct { usingnamespace w.kernel32; };
+const w = struct {
+   usingnamespace @import("std").os.windows;
+};
+const k32 = struct {
+   usingnamespace w.kernel32;
+};
 
-// this should remove the zero but it doesn't...
-fn nz(comptime s: []const u8) *const [s.len - 1]u8 {
-   return s[0..s.len - 1];
+// remove the sentinel value
+fn nz(comptime s: []const u8) *const [s.len]u8 {
+   return &@as([s.len]u8, s[0..s.len].*);
 }
 
 export fn start() void {
    const hStdout = k32.GetStdHandle(w.STD_OUTPUT_HANDLE);
-   const greeting = comptime nz("Hello, World!");
+   const greeting = nz("Hello, World!");
    _ = k32.WriteFile(hStdout.?, greeting, greeting.len, null, null);
    k32.ExitProcess(0);
 }
